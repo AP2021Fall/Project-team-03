996b4456eaafdff47bf75190c9269b5cf6ef9cbc
iyasi79 <73696676+iyasi79@users.noreply.github.com>
2021-12-05 17:24:17 +0330

    An enum has added to the project

--- main/src/main/java/model/Task.java
+++ main/src/main/java/model/Task.java
@@ -7,17 +7,15 @@ public class Task {
     private String title;
     private User assignee;
     private LocalDateTime deadline;
+    private static ArrayList<Task> tasks = new ArrayList<>();
 
     public Task(String title) {
+        generateId();
         this.title = title;
     }
 
     public String getId() {
         return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
     }
 
     public String getTitle() {
@@ -39,6 +37,25 @@ public class Task {
     public void setDeadline(LocalDateTime deadline) {
         this.deadline = deadline;
     }
+
+    private void generateId(){
+        if(tasks.isEmpty()){
+            this.id = 1;
+        } else{
+            int lastInd = tasks.size() - 1;
+            int lastId = tasks.get(lastInd);
+            this.id = lastId + 1;
+        }
+    }
+
+    public boolean changeTitle(int id, String title){
+        tasks.for
+    }
+
+    public boolean changeDescription(int id, String title){
+
+    }
+
 
     @Override
     public String toString() {
