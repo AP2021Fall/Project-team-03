546b6598afe8e8ce6430818cd5c714732dc91a72
[Saba Shamekhi] <[shamekhi.s2020@gmail.com]>
2021-12-06 15:09:56 +0330

    New changelist

--- main/src/main/java/view/TestCommit1.java
+++ main/src/main/java/view/TestCommit1.java
@@ -10,7 +10,5 @@ public class TestCommit1{
     public WelcomeMenu() {
         System.out.println("* Welcome to Trello *");
     }
-    public WelcomeMenu() {
-        System.out.println("* Welcome to Trello *");
-    }
+
 }
