04ecd25383f79c7041dd1694eb408584476d77c2
mohammad reza <94246666+Shapoori7@users.noreply.github.com>
2021-12-06 21:10:30 +0330

    last edit

---
+++ main/src/main/java/model/Admin.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Admin extends User{
+    public Admin(String name, String username, String password, String email) {
+        super(name, username, password, email);
+    }
+
+
+}
