e9a5ad4ded502220e7af9183da2ff1828878f5c6
[Saba Shamekhi] <[shamekhi.s2020@gmail.com]>
2021-12-14 11:00:05 +0330

    New changelist

--- main/src/main/java/controller/TeamMenuController.java
+++ main/src/main/java/controller/TeamMenuController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import model.ScoreBoard;
 import model.User;
 
 
@@ -43,6 +44,38 @@ public class TeamMenuController {
             if (user.getAllUsers().get(user))
                 return user;
         }return null;
+    }
+
+    public void showTeams(User user){
+        if(user.equals(findLoggedInUSer())) {
+            if (!isLoggedInUSerAdmin()) {
+
+                for (int i = 0; i < user.getUserTeams().size(); i++) {
+                    int teamNumber = i + 1;
+                    System.out.println(teamNumber + " < " + user.getUserTeams().get(i) + " > ");
+                }
+
+            }
+        }
+
+
+
+
+    }
+    public void showScoreBoard (String teamName){
+        for(ScoreBoard scoreBoard: ScoreBoard.getAllScoreBoards()){
+            if(scoreBoard.getTeamName().equals(teamName)){
+                int counter= 0;
+                for (String s :  scoreBoard.getScoreBoardInfoByTeamName(teamName).keySet()){
+                    //  scores.add(test.get(s));
+                    counter++;
+                    System.out.println(counter+". " + s + " : " + scoreBoard.getScoreBoardInfoByTeamName(teamName).get(s)+"");
+
+                }
+            }
+
+        }
+
     }
 
 
