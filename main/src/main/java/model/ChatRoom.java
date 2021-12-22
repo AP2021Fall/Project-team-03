3934dd8a11c510aa8ad51f5b1144a24a37c19a33
[Saba Shamekhi] <[shamekhi.s2020@gmail.com]>
2021-12-22 22:56:07 +0330

    team menu show command

--- main/src/main/java/model/ChatRoom.java
+++ main/src/main/java/model/ChatRoom.java
@@ -1,13 +1,22 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 public class ChatRoom {
     //TO have a list of all chatRooms
     private static List<ChatRoom> chatRoomList =new ArrayList<>();
     public static Integer chatRoomID;
+    public String senderName;
+    public String massageText;
+    LinkedHashMap<String,String>allTeamMassages;
 
+    public ChatRoom(String teamName) {
+        this.teamName=teamName;
+        this.allTeamMassages =new LinkedHashMap<>();
+    }
     // private static List<Massage> massagesOfTheTeam = new ArrayList<>();
 
     private String teamName;
@@ -18,3 +27,4 @@ public class ChatRoom {
         return chatRoomList;
     }
 }
+
