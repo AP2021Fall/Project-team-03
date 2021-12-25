ffd63063a0f6f3bfc055ecb3f736a2a69140d4bb
[Saba Shamekhi] <[shamekhi.s2020@gmail.com]>
2021-12-25 19:54:16 +0330

    chatroom

--- main/src/main/java/model/ChatRoom.java
+++ main/src/main/java/model/ChatRoom.java
@@ -11,20 +11,15 @@ public class ChatRoom {
     public static Integer chatRoomID;
     public String senderName;
     public String massageText;
-    LinkedHashMap<String,String>allTeamMassages;
+    LinkedHashMap<String,String>allTeamMassages= new LinkedHashMap<>();
 
-    public ChatRoom(String teamName) {
-        this.teamName=teamName;
-        this.allTeamMassages =new LinkedHashMap<>();
+    public LinkedHashMap<String, String> getAllTeamMassages() {
+        return allTeamMassages;
     }
-    // private static List<Massage> massagesOfTheTeam = new ArrayList<>();
 
     private String teamName;
-    //assign the name of the team from team class
-
 
     public static List<ChatRoom> getChatRoomList() {
         return chatRoomList;
     }
 }
-
