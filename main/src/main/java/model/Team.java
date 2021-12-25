18462ffe506dc6ed09b2ef48cd4e1a08d5f8381b
[Saba Shamekhi] <[shamekhi.s2020@gmail.com]>
2021-12-25 19:49:50 +0330

    team

--- main/src/main/java/model/Team.java
+++ main/src/main/java/model/Team.java
@@ -4,53 +4,38 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Team {
-    private int id;
     public static ArrayList<Team> teams = new ArrayList<Team>();
     public static ArrayList<Team> pendingTeams = new ArrayList<Team>();
 
-    //
+    // //*
     ScoreBoard teamsScoreBoard ;
     RoadMap teamRoadMap;
     ChatRoom chatRoom;
     private String teamName;
+
     private String teamLeaderName;
     public static HashMap<Team,Boolean> allTeams=new HashMap<>();// to see if the team is entered or not
 
+    private ArrayList<String> teamMembers = new ArrayList<String>();// the string is username
+    private ArrayList<Board> teamsBoards=new ArrayList<>();
+    private ArrayList<Task>  teamsTasks = new ArrayList<>();
+    //
 
-    private ArrayList<User> teamMembers;
-    private ArrayList<Board> teamsBoards;
-    private static ArrayList<Task>  teamsTasks = new ArrayList<>();
 
     public Team(String teamName) {
         this.teamName = teamName;
         allTeams.put(this,false);
     }
+
     public static HashMap<Team, Boolean> getAllTeams() {
         return allTeams;
     }
 
-    public Team() {
-        this.teamMembers = new ArrayList<User>();
-        this.teamsBoards = new ArrayList<Board>();
-    }
-
     public static Team getTeamById(int id) {
         for (Team team: teams)
-            if (team.getTeamId() == id)
-                return team;
+            //    if (team.getId() == id)
+            return team;
         return null;
-    }
-
-    public int getTeamId() {
-            if(allTeams.isEmpty()) {
-                this.id = 0;
-            }else {
-                int lastIndex=allTeams.size()-1;
-                int lastId=allTeams.get(lastIndex).getTeamId();
-                this.id=lastId+1;
-
-            }
-            return id;
     }
     public static Team getTeamByName(String teamName){
         for (Team team:teams){
@@ -69,12 +54,10 @@ public class Team {
         return pendingTeams;
     }
 
-
-
-    public ArrayList<User> getTeamMembers() {
+    public ArrayList<String> getTeamMembers() {
         return teamMembers;
     }
-   // public ArrayList<String>getTeamMembersByTeamName (String teamName) {return teamMembers;}
+    public ArrayList<String>getTeamMembersByTeamName (String teamName) {return teamMembers;}
 
 
     public ArrayList<Board> getTeamsBoards() {
@@ -96,7 +79,7 @@ public class Team {
     public ScoreBoard getTeamScoreBoard() {
         return teamsScoreBoard;
     }
-    //TODO fix this method : show task in team
+
     public RoadMap getTeamRoadMap() {
         return teamRoadMap;
     }
@@ -105,7 +88,7 @@ public class Team {
         return chatRoom;
     }
 
-    public String showTasks() {
-        return null;
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
     }
 }
