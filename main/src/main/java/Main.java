c8a84d316c334becd1cc0a4f1eea3031935ecb5d
[Saba Shamekhi] <[shamekhi.s2020@gmail.com]>
2022-01-28 19:58:54 +0330

    main

--- main/src/main/java/Main.java
+++ main/src/main/java/Main.java
@@ -1,5 +1,11 @@
+import view.MainMenu;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello this is a test");
+
+        MainMenu mainMenu =new MainMenu("MainMenu", null);
+        mainMenu.show();
+        mainMenu.execute();
     }
 }
