a6a7c82ad4e88aac5ac18cdc2b2912e17a41e7e4
IyasI <yasamin449@gmail.com>
2022-02-01 17:37:45 +0330

    login graphics

---
+++ src/main/java/com/example/project03withfx/fxView/login.java
@@ -0,0 +1,14 @@
+package com.example.project03withfx.fxView;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class login {
+    public TextField usernameField;
+    public TextField passwordField;
+    public Button loginButton;
+
+    public void login(ActionEvent actionEvent) {
+    }
+}
