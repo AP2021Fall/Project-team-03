d795dda94e50bfea8965952fc7ec4d92910e7e6f
[Saba Shamekhi] <[shamekhi.s2020@gmail.com]>
2022-02-02 14:00:26 +0330

    Register

--- src/main/java/com/example/project03withfx/fxView/Register.java
+++ src/main/java/com/example/project03withfx/fxView/Register.java
@@ -1,9 +1,17 @@
 package com.example.project03withfx.fxView;
 
+import com.example.project03withfx.model.Role;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static com.example.project03withfx.fxView.Menu.databaseController;
 
 public class Register {
     public TextField UsernameField;
@@ -14,18 +22,60 @@ public class Register {
     public TextField FullnameField;
     public Button RegisterButtom;
     public Button BackButtom;
-
+    public Label errorLabel;
+    public BorderPane pane;
     public void register(ActionEvent actionEvent) {
-        String username =UsernameField.getText();
-        String password1 =PasswordField.getText();
-        String password2 =PasswordConfirmationField.getText();
+        String username = UsernameField.getText();
+        String password1 = PasswordField.getText();
+        String password2 = PasswordConfirmationField.getText();
         String Birthday = BirthdayField.getText();
+        String email = EmailField.getText();
         String fullname = FullnameField.getText();
+        //
+        String roll = String.valueOf(Role.MEMBER);
+        if (username.isEmpty() || password1.isEmpty() || password2.isEmpty() || Birthday.isEmpty() || email.isEmpty() || fullname.isEmpty()) {
+            errorLabel.setText("please fill in the boxes first !");
+            return;
 
+        } else {
+            if (!password1.equals(password2)) {
+                System.err.println("Your passwords are not the same!");
+                errorLabel.setText("Your passwords are not the same!");
 
+                //   nextMenu = this;
+            } else {
+                //tabeye register
+                String state = databaseController.register(fullname, username, password1, Birthday, email, roll);
+                if (state.equalsIgnoreCase("This username is already registered")) {
+                    System.err.println(state);
+                    errorLabel.setText("This username is already taken");
+                    //nextMenu = this;
+                } else if (state.equalsIgnoreCase("Select the password in the following format")) {
+                    System.err.println("Select the password in the following format\n" +
+                            "Minimum 1 and maximum 3 digits at the beginning" +
+                            " - one big character - minimum 3 and maximum 7 small characters");
+                    errorLabel.setText("Select the password in the following format");
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("wrong password format");
+                    alert.setContentText(" Right format :Minimum 1 and maximum 3 digits at the beginning - one big character - minimum 3 and maximum 7 small characters");
+                    alert.showAndWait();
+                    //   nextMenu = this;
+                } else if (state.equalsIgnoreCase("Your registration was successful!")) {
+                    System.out.println(state);
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("register");
+                    alert.setContentText("register successfully");
+                    alert.showAndWait();
+                    // nextMenu = this.parent.subMenus.get(4);
+                }
+            }
+
+        }
     }
 
-    public void goToPreviousMenu(ActionEvent actionEvent) {
+    public void goToPreviousMenu(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        ((Stage) pane.getScene().getWindow()).setScene(new Scene(root));
 
     }
 }
